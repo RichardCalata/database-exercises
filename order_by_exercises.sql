5f4cffd853e7798343d797374ffa68fcb1083d1a
richardcalata <calatar6@gmail.com>
2018-05-23 16:17:38 -0500

    order by

--- order_by_exercises.sql
+++ order_by_exercises.sql
@@ -1,4 +1,10 @@
 use employees;
 
-select * from employees where first_name ='irena' order by birth_date and gender ='f';
+SELECT * FROM employees WHERE first_name ='irena' ORDER BY birth_date AND gender ='f';
 
+SELECT * FROM employees WHERE first_name IN ('irena','vidya','maya') ORDER BY last_name, first_name;
+
+SELECT * FROM employees
+WHERE hire_date LIKE '199%'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date DESC, hire_date ASC;
