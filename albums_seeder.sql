82752ab71c72fedc98fdf842291a949f49cdcded
richardcalata <calatar6@gmail.com>
2018-05-23 07:53:17 -0500

    select and update added

--- albums_seeder.sql
+++ albums_seeder.sql
@@ -1,8 +1,9 @@
 use codeup_test_db;
 truncate albums;
 insert into albums (artist, name,release_date,genre,sales)
-values ('Michael Jackson', 'Thriller', 1982,   'Pop, Rock, R&B',   47.3),(
-'AC/DC',   'Back in Black',    1980,   'Hard rock',    26.1),
+values
+('Michael Jackson', 'Thriller', 1982,   'Pop, Rock, R&B',   47.3),
+('AC/DC',   'Back in Black',    1980,   'Hard rock',    26.1),
 ('Pink Floyd',  'The Dark Side of the Moon',    1973,   'Progressive rock', 24.2),
 ('Meat Loaf',   'Bat Out of Hell',  1977,   'Hard rock, Progressive rock',  21.5),
 ('Whitney Houston / Various artists',   'The Bodyguard',    1992,   'R&B, Soul, Pop, Soundtrack',   28.4),
---
+++ update_exercises.sql
@@ -0,0 +1,9 @@
+use codeup_test_db;
+
+select * from albums;
+
+select * from albums where release_date < 1980;
+
+select * from albums where artist = 'michael jackson';
+
+update albums set artist = 'peter jackson' where id = 1,11,19
