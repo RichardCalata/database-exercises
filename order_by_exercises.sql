bb4969b48e595ae54a1a56fc991ee2595e3744bb
richardcalata <calatar6@gmail.com>
2018-05-23 15:55:15 -0500

    order by

---
+++ order_by_exercises.sql
@@ -0,0 +1,4 @@
+use employees;
+
+select * from employees where first_name ='irena' order by birth_date and gender ='f';
+
--- where_exercises.sql
+++ where_exercises.sql
@@ -1,7 +1,8 @@
 use employees;
 
-select * from employees where first_name in ('irina', 'maya', 'vidya');
-
+SELECT * FROM employees WHERE first_name IN ('irena', 'maya', 'vidya');
 SELECT * FROM employees WHERE last_name LIKE 'e%';
-
-SELECT * FROM employees WHERE hire_date LIKE '199%' and birth_date LIKE '%-12-25';
+SELECT * FROM employees WHERE hire_date LIKE '199%' AND birth_date LIKE '%-12-25';
+SELECT * FROM employees WHERE first_name= 'irena' OR first_name='vidya' OR first_name='maya' AND gender ='m';
+SELECT * FROM employees WHERE last_name LIKE 'e%' OR last_name LIKE '%e';
+SELECT * FROM employees WHERE last_name LIKE '%q%' and last_name not like '%qu%';
