7a978840439120a24a8b33f1c34678396fd419ac
richardcalata <calatar6@gmail.com>
2018-05-24 14:57:57 -0500

    unique

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,7 +1,5 @@
 USE codeup_test_db;
-
-drop table if exists albums;
-
+DROP TABLE IF EXISTS albums;
 CREATE TABLE albums(
 id int unsigned not null auto_increment,
 primary key(id),
@@ -9,6 +7,13 @@ artist varchar(100),
 name varchar(100),
 release_date smallint unsigned,
 genre varchar (100),
-sales float (10,2)
+sales float (10,2),
+unique (artist,name)
 );
+
+
+
+
+
+
 
