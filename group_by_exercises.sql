75e2d046a18e2a0494e856e069c72b2972b7e70a
richardcalata <calatar6@gmail.com>
2018-05-24 14:06:42 -0500

    aliases

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -2,4 +2,10 @@ USE employees;
 
 
 SELECT first_name, last_name, count(*) FROM employees where last_name LIKE 'e%e'
-GROUP BY first_name, last_name ORDER BY count(*)desc;
+GROUP BY first_name, last_name ORDER BY count(*)desc, last_name;
+
+SELECT last_name
+FROM employees
+WHere last_name Like '%q%'
+and last_name NoT LIKE '%qu%';
+GROUP BY last_name;
