4fbaa3477b9609502012ced0dc70ab4ba854d67d
richardcalata <calatar6@gmail.com>
2018-05-29 14:30:32 -0500

    bonus 1

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -14,13 +14,27 @@ select title from titles;
 
 select * from employees where first_name = 'Aamod';
 
-select CONCAT(last_name, ', ', first_name) AS Name from employees
-where emp_no in (
-
-select emp_no from dept_manager
-where to_date > now()
-)
-
-and gender ='f';
+select CONCAT(first_name,' ' ,last_name) AS Name FROM employees
+WHERE emp_no
+IN (SELECT emp_no FROM dept_manager
+WHERE to_date > now())
+AND gender ='f';
 
 
+
+select dept_name from departments
+where dept_no
+IN (select dept_no from dept_manager
+WHERE emp_no IN
+(SELECT emp_no from employees WHERE gender = 'f')
+AND to_date > now());
+
+select emp_no from dept_manager where to_date >now();
+
+select last_name, emp_no from employees
+where emp_no in (select emp_no from dept_manager where to_date >now())and gender = 'f';
+
+
+SELECT emp_no from dept_manager WHERE to_date > now()
+  AND  emp_no IN
+(SELECT emp_no from employees WHERE gender = 'f');
