2e06d5ec79dcf503f821ace835ad4317e3c37714
richardcalata <calatar6@gmail.com>
2018-05-29 13:30:48 -0500

    subqueries 2

--- subqueries_exercises.sql
+++ subqueries_exercises.sql
@@ -5,5 +5,13 @@ select CONCAT(last_name, ', ',first_name), hire_date from employees
 where hire_date = (select hire_date from employees where emp_no = 101010);
 
 
+select title from titles where emp_no in
 
-select
+(select emp_no from employees where first_name = 'Aamod');
+
+
+select title from titles;
+
+select * from employees where first_name = 'Aamod';
+
+
