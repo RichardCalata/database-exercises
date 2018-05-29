3e6e0648f45a238ff2c36a5d3db444ed8477ca7d
richardcalata <calatar6@gmail.com>
2018-05-29 10:55:07 -0500

    world exercise 2

--- world-ex.sql
+++ world-ex.sql
@@ -6,13 +6,21 @@ use world;
 show tables;
 
 describe city;
+select * from city limit 2;
 
 describe country;
 select * from country limit 1;
 
 describe countrylanguage;
+select * from countrylanguage limit 2;
 
-select language from countrylanguage
+select language, countrylanguage.percentage from countrylanguage
 join country on country.code = countrylanguage.countrycode
   join city on city.countrycode = country.code
-  where city.name Like 'santa monica';
+  where city.name Like 'santa monica'
+order by countrylanguage.percentage;
+
+select region, count(*) from country
+GROUP BY region
+ORDER BY count(*);
+
